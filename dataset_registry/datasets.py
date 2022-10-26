e586939cf3d2d3515e05dac1a0a613aa05a687b3
eelismie <evmielonen@gmail.com>
2022-10-26 18:36:16 +0200

    added Visda2017 into dataset registry

--- dataset_registry/datasets.py
+++ dataset_registry/datasets.py
@@ -1,9 +1,91 @@
-from .mnistm import MNISTM
-from torch.utils.data import DataLoader
-from torchvision import datasets
-import torch
-import torchvision.transforms as transforms
 import os
+import timm
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision.transforms as T
+import torchvision.transforms as transforms
+
+from .mnistm import MNISTM
+from torchvision import datasets
+from torch.utils.data import DataLoader
+from tllib.vision.transforms import ResizeImage
+from tllib.vision.datasets.visda2017 import VisDA2017
+from timm.data.auto_augment import auto_augment_transform, rand_augment_transform
+
+def get_train_transform(resizing='default', scale=(0.08, 1.0), ratio=(3. / 4., 4. / 3.), random_horizontal_flip=True,
+                        random_color_jitter=False, resize_size=224, norm_mean=(0.485, 0.456, 0.406),
+                        norm_std=(0.229, 0.224, 0.225), auto_augment=None):
+    """
+    resizing mode:
+        - default: resize the image to 256 and take a random resized crop of size 224;
+        - cen.crop: resize the image to 256 and take the center crop of size 224;
+        - res: resize the image to 224;
+    """
+    transformed_img_size = 224
+    if resizing == 'default':
+        transform = T.Compose([
+            ResizeImage(256),
+            T.RandomResizedCrop(224, scale=scale, ratio=ratio)
+        ])
+    elif resizing == 'cen.crop':
+        transform = T.Compose([
+            ResizeImage(256),
+            T.CenterCrop(224)
+        ])
+    elif resizing == 'ran.crop':
+        transform = T.Compose([
+            ResizeImage(256),
+            T.RandomCrop(224)
+        ])
+    elif resizing == 'res.':
+        transform = ResizeImage(resize_size)
+        transformed_img_size = resize_size
+    else:
+        raise NotImplementedError(resizing)
+    transforms = [transform]
+    if random_horizontal_flip:
+        transforms.append(T.RandomHorizontalFlip())
+    if auto_augment:
+        aa_params = dict(
+            translate_const=int(transformed_img_size * 0.45),
+            img_mean=tuple([min(255, round(255 * x)) for x in norm_mean]),
+            interpolation=Image.BILINEAR
+        )
+        if auto_augment.startswith('rand'):
+            transforms.append(rand_augment_transform(auto_augment, aa_params))
+        else:
+            transforms.append(auto_augment_transform(auto_augment, aa_params))
+    elif random_color_jitter:
+        transforms.append(T.ColorJitter(brightness=0.5, contrast=0.5, saturation=0.5, hue=0.5))
+    transforms.extend([
+        T.ToTensor(),
+        T.Normalize(mean=norm_mean, std=norm_std)
+    ])
+    return T.Compose(transforms)
+
+
+def get_val_transform(resizing='default', resize_size=224,
+                      norm_mean=(0.485, 0.456, 0.406), norm_std=(0.229, 0.224, 0.225)):
+    """
+    resizing mode:
+        - default: resize the image to 256 and take the center crop of size 224;
+        - res.: resize the image to 224
+    """
+    if resizing == 'default':
+        transform = T.Compose([
+            ResizeImage(256),
+            T.CenterCrop(224),
+        ])
+    elif resizing == 'res.':
+        transform = ResizeImage(resize_size)
+    else:
+        raise NotImplementedError(resizing)
+    return T.Compose([
+        transform,
+        T.ToTensor(),
+        T.Normalize(mean=norm_mean, std=norm_std)
+    ])
 
 class MNIST():
 
@@ -11,7 +93,7 @@ class MNIST():
         self.path = path 
         self.opt = opt
         
-    def get_loaders(self):
+    def get_loaders(self, train_transform_args = {}, val_transform_args = {}):
 
         opt = self.opt
 
@@ -22,8 +104,8 @@ class MNIST():
                 self.path,
                 train=True,
                 download=True,
-                transform=transforms.Compose(
-                    [transforms.Resize(opt["img_size"]), transforms.ToTensor(), transforms.Normalize([0.5], [0.5])]
+                transform=T.Compose(
+                    [T.Resize(opt["img_size"]), T.ToTensor(), T.Normalize([0.5], [0.5])]
                 ),
             ),
             batch_size=opt["batch_size"],
@@ -35,10 +117,10 @@ class MNIST():
                 self.path,
                 train=False,
                 download=True,
-                transform=transforms.Compose(
-                    [transforms.Resize(opt["img_size"]),
-                    transforms.ToTensor(),
-                    transforms.Normalize([0.5], [0.5])
+                transform=T.Compose(
+                    [T.Resize(opt["img_size"]),
+                    T.ToTensor(),
+                    T.Normalize([0.5], [0.5])
                     ]
                 ),
             ),
@@ -55,7 +137,7 @@ class MNIST_M():
         self.path = path 
         self.opt = opt
         
-    def get_loaders(self):
+    def get_loaders(self, train_transform_args = {}, val_transform_args = {}):
 
         opt = self.opt
 
@@ -65,11 +147,11 @@ class MNIST_M():
                 "data/mnistm",
                 train=True,
                 download=True,
-                transform=transforms.Compose(
+                transform=T.Compose(
                     [
-                        transforms.Resize(opt["img_size"]),
-                        transforms.ToTensor(),
-                        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+                        T.Resize(opt["img_size"]),
+                        T.ToTensor(),
+                        T.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
                     ]
                 ),
             ),
@@ -82,11 +164,11 @@ class MNIST_M():
                 "data/mnistm",
                 train=False,
                 download=True,
-                transform=transforms.Compose(
+                transform=T.Compose(
                     [
-                        transforms.Resize(opt["img_size"]),
-                        transforms.ToTensor(),
-                        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+                        T.Resize(opt["img_size"]),
+                        T.ToTensor(),
+                        T.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
                     ]
                 ),
             ),
@@ -95,3 +177,56 @@ class MNIST_M():
         )
 
         return train_loader, test_loader
+
+class VISDA17_real:
+
+    def __init__(self,path,opt = {}): 
+        self.path = path
+        self.opt = opt 
+
+    def get_loaders(self, train_transform_args = {}, val_transform_args = {}):
+
+        opt = self.opt
+
+        train_transform = get_train_transform(**train_transform_args)
+        test_transform = get_val_transform(**val_transform_args)
+        dataset = VisDA2017(self.path, 'Real', download=True)
+
+        train_size = int(0.8 * len(dataset))
+        test_size = len(dataset) - train_size
+        train, test = torch.utils.data.random_split(dataset, [train_size, test_size])
+
+        train.transform = train_transform
+        test.transform = test_transform
+
+        train_loader = DataLoader(train, batch_size=opt["batch_size"], shuffle=True, drop_last=True)
+        test_loader = DataLoader(test, batch_size=opt["batch_size"], shuffle=False)
+
+        return train_loader, test_loader
+
+class VISDA17_synthetic:
+
+    def __init__(self,path,opt = {}): 
+        self.path = path
+        self.opt = opt 
+
+    def get_loaders(self, train_transform_args = {}, val_transform_args = {} ):
+
+        opt = self.opt
+
+        train_transform = get_train_transform(**train_transform_args)
+        test_transform = get_val_transform(**val_transform_args)
+        dataset = VisDA2017(self.path, 'Synthetic', download=True)
+
+        train_size = int(0.8 * len(dataset))
+        test_size = len(dataset) - train_size
+        train, test = torch.utils.data.random_split(dataset, [train_size, test_size])
+
+        train.transform = train_transform
+        test.transform = test_transform
+
+        train_loader = DataLoader(train, batch_size=opt["batch_size"], shuffle=True, drop_last=True)
+        test_loader = DataLoader(test, batch_size=opt["batch_size"], shuffle=False)
+
+        return train_loader, test_loader
+
