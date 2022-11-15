beeffe5fdafa4f5b009cffb8fe2f52f0e316ac6e
eelismie <evmielonen@gmail.com>
2022-11-15 12:05:09 +0100

    added photo -> cartoon dataset + experiments

--- launch.py
+++ launch.py
@@ -3,15 +3,20 @@ import os
 base_path = "/home/mielonen/robust-domain-adaptation"
 
 experiment_list = [
+    "experiment_registry/daln-pacs-c.yaml",
+    "experiment_registry/mcc-pacs-c.yaml",
+    "experiment_registry/mdd-pacs-c.yaml",
+    "experiment_registry/daln-pacs.yaml",
+    "experiment_registry/mcc-pacs.yaml",
+    "experiment_registry/mdd-pacs.yaml",
+    "experiment_registry/daln-visda2017.yaml",
     "experiment_registry/mcc-visda2017.yaml",
-    "experiment_registry/mdd-visda2017.yaml", 
-    "experiment_registry/mcc-pacs.yaml", 
-    "experiment_registry/mdd-pacs.yaml"
+    "experiment_registry/mdd-visda2017.yaml"
 ]
 
 def modify(yaml, key, value):
     """
-    TODO : take dict in yaml and change value in key to value (good for setting experiment args)
+    TODO : take dict in yaml and change value in key to value
     """
     pass 
 
